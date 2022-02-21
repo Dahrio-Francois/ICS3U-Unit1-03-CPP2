37d20e1eb28a5f90b6ec6120800c42ed19720cde
Dahrio-Francois <dahrio.francois@mths.ca>
2022-02-21 21:54:04 +0000

    added HelloWorld.cpp

---
+++ HelloWorld!.cpp
@@ -0,0 +1,11 @@
+// Copyright (c) 2022 Dahrio Francois All rights reserved
+//
+// Created by: Dahrio Francois
+// Created on: Feb 2022
+// This is the "Hello, World!" program, with proper style
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+}
